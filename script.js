aaacfdab7e27ac0a1c1379131417ed7286acdca2
Reasho <106495283+Reasho@users.noreply.github.com>
2026-05-09 21:03:23 -0400

    added the function for the computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello World")
+
+function getComputerChoice() {
+    let randomNum = Math.random();
+
+    if (randomNum <0.33) {
+        return "Rock";
+    } else if (randomNum < 0.66) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+console.log(getComputerChoice())
